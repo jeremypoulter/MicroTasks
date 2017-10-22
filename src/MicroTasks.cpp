21216a9653692a7cd0627a837fe7bef4c3a5ea0b
Jeremy Poulter <jeremy@bigjungle.net>
2017-10-22 18:43:47 +0100

    Added some debug

--- src/MicroTasks.cpp
+++ src/MicroTasks.cpp
@@ -6,6 +6,8 @@
 #include "MicroTasksEvent.h"
 #include "MicroTasks.h"
 
+#include "debug.h"
+
 using namespace MicroTasks;
 
 uint32_t MicroTasksClass::WaitForEvent = (1 << 31);
@@ -28,7 +30,6 @@ uint32_t MicroTasksClass::update()
   Event *oNextEvent;
   EventListener *oNextEventListener;
   Task *oNextTask;
-  
   // Any events triggered?
   for (Event *oEvent = (Event *)Event::oEvents.GetFirst(); oEvent; oEvent = oNextEvent)
   {
@@ -50,46 +51,49 @@ uint32_t MicroTasksClass::update()
   }
   
   // Any tasks waiting to be woken
+  uint32_t uiNextEvent = 0xFFFFFFFF;
   for (Task *oTask = (Task *)oTasks.GetFirst(); oTask; oTask = oNextTask)
   {
-    // Keep a pointer to the next task in case this on is stopped
+    // Keep a pointer to the next task in case this one is stopped
     oNextTask = (Task *)oTask->GetNext();
     if (oTask->ulNextLoop <= millis()) {
       wakeTask(oTask, WakeReason_Scheduled);
     }
-  }
-  
-  // Find the time of the next event
-  uint32_t uiNextEvent = 0xFFFFFFFF;
-  for (Task *oTask = (Task *)oTasks.GetFirst(); oTask; oTask = (Task *)oTask->GetNext()) {
     if(oTask->ulNextLoop < uiNextEvent) {
       uiNextEvent = oTask->ulNextLoop;
     }
   }
+
+  DEBUG_PORT.flush();
   return uiNextEvent;
 }
 
 void MicroTasksClass::wakeTask(Task *oTask, WakeReason eReason)
 {
-//   Serial.print(millis());
-//   Serial.print(": W ");
-//   Serial.print((unsigned int)oTask);
-//   Serial.print(" [");
-//   Serial.print((int)eReason);
-//   Serial.print("] -> ");
+  DBUG(millis());
+  DBUG(": W ");
+  DBUG((unsigned int)oTask);
+  DBUG(" [");
+  DBUG((int)eReason);
+  DBUG("] -> ");
 
+#ifdef ENABLE_DEBUG
+  uint32_t ulStart = micros();
+#endif
   uint32_t ulDelay = oTask->loop(eReason);
+  DBUG(micros() - ulStart);
+  DBUG(":");
   uint32_t ulNext = ulDelay & ~MicroTask.WaitForMask;
 
   oTask->uiFlags = ulDelay & MicroTask.WaitForMask;
   if (MicroTask.Infinate == ulNext) {
     oTask->ulNextLoop = 0xFFFFFFFF;
-//     Serial.println("Forever");
+    DBUGLN("Forever");
   } else {
     oTask->ulNextLoop = millis() + ulNext;
-//     Serial.print(ulNext);
-//     Serial.print(":");
-//     Serial.println(oTask->ulNextLoop);
+    DBUG(ulNext);
+    DBUG(":");
+    DBUGLN(oTask->ulNextLoop);
   }
 }
 
@@ -102,6 +106,7 @@ void MicroTasksClass::startTask(Task *oTask)
 void MicroTasksClass::stopTask(Task *oTask)
 {
   oTasks.Remove(oTask);
+  oTask->ulNextLoop = 0xFFFFFFFF;
 }
 
 MicroTasksClass MicroTask;
