e4407873d11ca76abcc526198e3e3343d6f25a43
Jeremy Poulter <jeremy@bigjungle.net>
2018-02-08 00:02:32 +0000

    Hack to allow testing of the library code

---
+++ src/test.cpp
@@ -0,0 +1,9 @@
+#ifdef TEST_BLINK
+#include "../examples/Blink/Blink.ino"
+#endif
+#ifdef TEST_INTERRUPT
+#include "../examples/Interrupt/Interrupt.ino"
+#endif
+#ifdef TEST_ALARM
+#include "../examples/Alarm/Alarm.ino"
+#endif
