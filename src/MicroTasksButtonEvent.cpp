10c49ea92782c35620b8742f1ff264e1f5bc4360
Jeremy Poulter <jeremy@bigjungle.net>
2018-05-30 16:47:50 +0100

    Improved the Button debounce to ensure that the state returned by IsPressed always matches the state that triggered the event and also that the opposit event is not missed.

--- src/MicroTasksButtonEvent.cpp
+++ src/MicroTasksButtonEvent.cpp
@@ -6,12 +6,46 @@
 
 using namespace MicroTasks;
 
+ButtonEvent::ButtonEvent(uint8_t pin, int mode, int inputMode, int debounce) :
+  Interrupt(pin, CHANGE, inputMode), debounce(debounce), lastTime(0), state(false), mode(mode),
+  debounceAlarm(*this)
+{
+  pinMode(pin, inputMode);
+  state = LOW == digitalRead(pin);
+}
+
 void ButtonEvent::Trigger()
 {
   unsigned long timeNow = millis();
   if (timeNow >= lastTime + debounce)
   {
+    state = LOW == digitalRead(pin);
+    triggerInterrupt();
+  }
+  else 
+  {
+    debounceAlarm.Set(debounce, false);
+  }
+
+  lastTime = timeNow;
+}
+
+void ButtonEvent::ButtonTriggerDebounce::Trigger()
+{
+  bool newState = LOW == digitalRead(event.pin);
+  if(newState != event.state) 
+  {
+    event.state = newState;
+    event.lastTime = millis();
+    event.triggerInterrupt();
+  }
+}
+
+void ButtonEvent::triggerInterrupt()
+{
+  if((true == state && (RISING == mode || CHANGE == mode)) ||
+      (false == state && (FALLING == mode || CHANGE == mode)))
+  {
     Interrupt::Trigger();
   }
-  lastTime = timeNow;
 }
